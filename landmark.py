29e748218046651d70f85f581d1ff4e4268fc271
root <dandy.naufaldi@gmail.com>
2018-07-05 08:35:06 +0000

    Show detected and aligned face 1by1

--- landmark.py
+++ landmark.py
@@ -38,22 +38,33 @@ def shapeToNp(shape):
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_5_face_landmarks.dat')
 
-image = cv2.imread('faces/6.jpg')
+image = cv2.imread('faces/5.jpg')
 image = resizeImg(image)
 # bnw = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 bnw = image
 
 rects = detector(image, 1)
 print('Faces =', len(rects))
+
+# faces = dlib.full_object_detections()
+
 for (i, rect) in enumerate(rects):
 	shape = predictor(image, rect)
+	aligned = dlib.get_face_chip(image, shape, padding=0.5)
 	shape = shapeToNp(shape)
 	(left, top, right, bottom) = getPosFromRect(rect)
+	
 	cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 2)
-
+	cv2.putText(image, "Face #{}".format(i + 1), (left - 10, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 	for(x, y) in shape:
 		cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
-
-cv2.imshow('output', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+	
+	cv2.imshow('detect {}'.format(i+1), image)
+	cv2.imshow('align {}'.format(i+1), aligned)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+	continue
+	
+# cv2.imshow('output', image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
