d43d1d11bdc4519956d149f7bb8682c6a459d6c7
dandynaufaldi <dandy.naufaldi@gmail.com>
2018-07-11 10:37:41 +0700

    save memory on loading image

--- prepdata.py
+++ prepdata.py
@@ -8,10 +8,12 @@ def main(db_name):
 	data = loadfile.loadData('{}'.format(db_name), '{}.mat'.format(db_name))
 	data = preprocess.cleanData(data)
 	# data = data.iloc[:10]
-	data = preprocess.loadImage(data)
+	# data = preprocess.loadImage(data)
 	print('[PREPROC] Run face alignment...')
 	paths = data['full_path'].tolist()
-	for (image, path) in tqdm(zip(data['image'].tolist(), paths), total=len(paths)):
+	for (path) in tqdm(paths):
+		flname = os.path.join('{}_crop'.format(db_name), path[0])
+		image = cv2.imread(flname)
 		image = preprocess.getAlignedFace(image)
 		folder = os.path.join(RES_DIR, path[0].split('/')[0])
 		if not os.path.exists(folder):
