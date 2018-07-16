71ffa6fcbf65fa816d77b85b8b97291be74dce67
dandynaufaldi <dandy.naufaldi@gmail.com>
2018-07-16 11:29:55 +0700

    fix preprocess image on each model

--- model.py
+++ model.py
@@ -5,7 +5,6 @@ from keras.layers import Dense, Flatten, Dropout, GlobalAveragePooling2D
 from keras.models import Model
 from keras.utils.vis_utils import plot_model
 
-
 class AgenderNetVGG16(Model):
 	def __init__(self):
 		base = VGG16(
@@ -19,7 +18,7 @@ class AgenderNetVGG16(Model):
 		topLayer = Dropout(0.5)(topLayer)
 		genderLayer = Dense(2, activation='softmax', name='gender_prediction')(topLayer)
 		ageLayer = Dense(10, activation='softmax', name='age_prediction')(topLayer)
-		super(AgenderNetVGG16, self).__init__(inputs=base.input, outputs=[genderLayer, ageLayer], name='AgenderNetVGG16')
+		super().__init__(inputs=base.input, outputs=[genderLayer, ageLayer], name='AgenderNetVGG16')
 
 	def prepPhase1(self):
 		for layer in self.layers[:19]:
@@ -29,11 +28,17 @@ class AgenderNetVGG16(Model):
 		for layer in self.layers[11:]:
 			layer.trainable = True
 	
+	def setWeight(self, path):
+		self.load_weights(path)
+
 	@staticmethod
 	def prepImg(data):
 		data = data.astype('float32')
-		data /= 127.5
-		data -= 1.
+		data = data[..., ::-1]
+		mean = [103.939, 116.779, 123.68]
+		data[..., 0] -= mean[0]
+		data[..., 1] -= mean[1]
+		data[..., 2] -= mean[2]
 		return data
 
 class AgenderNetInceptionV3(Model):
@@ -45,7 +50,7 @@ class AgenderNetInceptionV3(Model):
 		topLayer = GlobalAveragePooling2D(name='avg_pool')(base.output)
 		genderLayer = Dense(2, activation='softmax', name='gender_prediction')(topLayer)
 		ageLayer = Dense(10, activation='softmax', name='age_prediction')(topLayer)
-		super(AgenderNetInceptionV3, self).__init__(inputs=base.input, outputs=[genderLayer, ageLayer], name='AgenderNetInceptionV3')
+		super().__init__(inputs=base.input, outputs=[genderLayer, ageLayer], name='AgenderNetInceptionV3')
 	
 	def prepPhase1(self):
 		for layer in self.layers[:311]:
@@ -55,12 +60,14 @@ class AgenderNetInceptionV3(Model):
 		for layer in self.layers[280:]:
 			layer.trainable = True
 
+	def setWeight(self, path):
+		self.load_weights(path)
+
 	@staticmethod
 	def prepImg(data):
 		data = data.astype('float32')
-		data /= 255.
-		data -= 0.5
-		data *= 2.
+		data /= 127.5
+		data -= 1.
 		return data
 
 class AgenderNetXception(Model):
@@ -72,7 +79,7 @@ class AgenderNetXception(Model):
 		topLayer = GlobalAveragePooling2D(name='avg_pool')(base.output)
 		genderLayer = Dense(2, activation='softmax', name='gender_prediction')(topLayer)
 		ageLayer = Dense(10, activation='softmax', name='age_prediction')(topLayer)
-		super(AgenderNetXception, self).__init__(inputs=base.input, outputs=[genderLayer, ageLayer], name='AgenderNetXception')
+		super().__init__(inputs=base.input, outputs=[genderLayer, ageLayer], name='AgenderNetXception')
 
 	def prepPhase1(self):
 		for layer in self.layers[:132]:
@@ -82,12 +89,14 @@ class AgenderNetXception(Model):
 		for layer in self.layers[:115]:
 			layer.trainable = True
 	
+	def setWeight(self, path):
+		self.load_weights(path)
+
 	@staticmethod
 	def prepImg(data):
 		data = data.astype('float32')
-		data /= 255.
-		data -= 0.5
-		data *= 2.
+		data /= 127.5
+		data -= 1.
 		return data
 
 if __name__ == '__main__':
@@ -102,4 +111,4 @@ if __name__ == '__main__':
 	# for (i, layer) in enumerate(model.layers):
 	# 	print(i, layer.name, layer.trainable)
 	
-	plot_model(model, to_file='AgenderNetXception.png')
+	# plot_model(model, to_file='AgenderNetXception.png')
